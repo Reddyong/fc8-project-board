0d1509a5d3dc32659c8a7916b918ef5baf49eac1
Reddyong <d0209hong@naver.com>
2024-07-12 11:51:13 +0900

    #1 - Querydsl 이용하여 api 검색 옵션 추가
    QuerydslPredicateExecutor,
    QuerydslBinderCustomizer 를 이용하여 간단하게 개발

--- src/main/java/com/fc8/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fc8/projectboard/repository/ArticleRepository.java
@@ -1,9 +1,32 @@
 package com.fc8.projectboard.repository;
 
 import com.fc8.projectboard.domain.Article;
+import com.fc8.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,    // entity 안의 모든 필드에 대한 검색기능 추가
+        QuerydslBinderCustomizer<QArticle>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+
+        // like '%${value}% 처럼 해당 문자열이 다른 문자열의 일부로 포함되어 있는지 확인 및 대소문자 구분 X
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
+
 }
--- src/main/java/com/fc8/projectboard/repository/CommentRepository.java
+++ src/main/java/com/fc8/projectboard/repository/CommentRepository.java
@@ -1,9 +1,30 @@
 package com.fc8.projectboard.repository;
 
 import com.fc8.projectboard.domain.Comment;
+import com.fc8.projectboard.domain.QComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends
+        JpaRepository<Comment, Long>,
+        QuerydslPredicateExecutor<Comment>,
+        QuerydslBinderCustomizer<QComment>
+{
+
+    @Override
+    default void customize(QuerydslBindings bindings, QComment root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createdAt, root.createdBy);
+
+        // like '%${value}% 처럼 해당 문자열이 다른 문자열의 일부로 포함되어 있는지 확인 및 대소문자 구분 X
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
