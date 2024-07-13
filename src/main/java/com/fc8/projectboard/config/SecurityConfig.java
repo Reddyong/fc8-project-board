8708583bc9da6f2eeaf3e2bd4d797aa939e01f3d
Reddyong <d0209hong@naver.com>
2024-07-13 16:29:31 +0900

    #19 - 불필요한 dependency import 제거

--- src/main/java/com/fc8/projectboard/config/SecurityConfig.java
+++ src/main/java/com/fc8/projectboard/config/SecurityConfig.java
@@ -2,9 +2,7 @@ package com.fc8.projectboard.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
