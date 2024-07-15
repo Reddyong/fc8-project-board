84474c04a7ba1ea9f95493a0413f09596f11ae67
Reddyong <d0209hong@naver.com>
2024-07-15 18:00:09 +0900

    #20 - User 엔티티와 Article, Comment 엔티티에 연관관계 적용
    테이블 필드 수정으로 영향 받은 `data.sql` 테스트 데이터도 업데이트

--- src/main/java/com/fc8/projectboard/domain/Article.java
+++ src/main/java/com/fc8/projectboard/domain/Article.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 import java.util.Set;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "title"),
         @Index(columnList = "hashtag"),
@@ -25,6 +25,10 @@ public class Article extends AuditingFields {
     private Long id;
 
     @Setter
+    @ManyToOne(optional = false)
+    private User user;
+
+    @Setter
     @Column(nullable = false)
     private String title;
 
@@ -36,21 +40,22 @@ public class Article extends AuditingFields {
     private String hashtag;
 
     @ToString.Exclude
-    @OrderBy("id")
+    @OrderBy("createdAt DESC")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<Comment> comments = new LinkedHashSet<>();
 
     protected Article() {
     }
 
-    private Article(String title, String content, String hashtag) {
+    private Article(User user, String title, String content, String hashtag) {
+        this.user = user;
         this.title = title;
         this.content = content;
         this.hashtag = hashtag;
     }
 
-    public static Article of(String title, String content, String hashtag) {
-        return new Article(title, content, hashtag);
+    public static Article of(User user, String title, String content, String hashtag) {
+        return new Article(user, title, content, hashtag);
     }
 
     @Override
