4d92d7010d9f6a29d727b18afacf6e7e83b61d8b
Reddyong <d0209hong@naver.com>
2024-07-17 22:02:24 +0900

    #40 - 게시판, 게시글 뷰의 기능에 대응하는 자바 코드 구현
    CRUD 기능 중, save, update, delete 에 관한 코드 구현

--- src/main/java/com/fc8/projectboard/controller/ArticleController.java
+++ src/main/java/com/fc8/projectboard/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package com.fc8.projectboard.controller;
 
-import com.fc8.projectboard.domain.type.SearchType;
-import com.fc8.projectboard.dto.ArticleDto;
+import com.fc8.projectboard.domain.constant.FormStatus;
+import com.fc8.projectboard.domain.constant.SearchType;
 import com.fc8.projectboard.dto.ArticleWithCommentsDto;
+import com.fc8.projectboard.dto.UserDto;
+import com.fc8.projectboard.dto.request.ArticleRequest;
 import com.fc8.projectboard.dto.response.ArticleResponse;
 import com.fc8.projectboard.dto.response.ArticleWithCommentResponse;
 import com.fc8.projectboard.service.ArticleService;
@@ -14,10 +16,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -53,7 +52,7 @@ public class ArticleController {
             ModelMap modelMap
     ) {
 
-        ArticleWithCommentsDto articleWithCommentsDto = articleService.getArticle(articleId);
+        ArticleWithCommentsDto articleWithCommentsDto = articleService.getArticleWithComments(articleId);
         ArticleWithCommentResponse articleWithCommentResponse = ArticleWithCommentResponse.from(articleWithCommentsDto);
 
         modelMap.addAttribute("article", articleWithCommentResponse);
@@ -63,7 +62,7 @@ public class ArticleController {
     }
 
     @GetMapping(path = "/search-hashtag")
-    public String searchHashtag(
+    public String searchArticleHashtag(
             @RequestParam(required = false) String searchValue,
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC)
             Pageable pageable,
@@ -79,5 +78,49 @@ public class ArticleController {
         modelMap.addAttribute("searchType", SearchType.HASHTAG);
 
         return "articles/search-hashtag";
+    }
+
+    @GetMapping(path = "/form")
+    public String articleForm(ModelMap modelMap) {
+        modelMap.addAttribute("formStatus", FormStatus.CREATE);
+
+        return "articles/form";
+    }
+
+    @PostMapping(path = "/form")
+    public String postNewArticle(ArticleRequest articleRequest) {
+        // TODO : 인증 정보를 넣어줘야 한다. -> 강의에서는 userId 를 PK 로 사용하고 있지만, 나는 auto increment 한 id 를 PK로 가지기 때문에 id 값 찾아오는 과정도 필요
+
+        articleService.saveArticle(articleRequest.toDto(UserDto.of(1L, "reddyong", "reddyong", "reddyong@email.com", "Reddyong", "memo", null, null, null, null)));
+
+        return "redirect:/articles";
+    }
+
+    @GetMapping(path = "/{articleId}/form")
+    public String updateArticleForm(@PathVariable Long articleId, ModelMap modelMap) {
+        ArticleResponse article = ArticleResponse.from(articleService.getArticle(articleId));
+
+        modelMap.addAttribute("article", article);
+        modelMap.addAttribute("formStatus", FormStatus.UPDATE);
+
+        return "articles/form";
+    }
+
+    @PostMapping(path = "/{articleId}/form")
+    public String updateArticle(@PathVariable Long articleId, ArticleRequest articleRequest) {
+        // TODO : 인증 정보를 넣어줘야 한다.
+        articleService.updateArticle(articleId, articleRequest.toDto(UserDto.of(
+                1L, "reddyong", "reddyong", "reddyong@email.com", "reddyong", "memo", null, null, null, null
+        )));
+
+        return "redirect:/articles/" + articleId;
+    }
+
+    @PostMapping(path = "/{articleId}/delete")
+    public String deleteArticle(@PathVariable Long articleId) {
+        // TODO : 인증 정보를 넣어줘야 한다.
+        articleService.deleteArticle(articleId);
+
+        return "redirect:/articles";
     }
 }
--- src/test/java/com/fc8/projectboard/service/ArticleServiceTest.java
+++ src/test/java/com/fc8/projectboard/service/ArticleServiceTest.java
@@ -2,11 +2,12 @@ package com.fc8.projectboard.service;
 
 import com.fc8.projectboard.domain.Article;
 import com.fc8.projectboard.domain.User;
-import com.fc8.projectboard.domain.type.SearchType;
+import com.fc8.projectboard.domain.constant.SearchType;
 import com.fc8.projectboard.dto.ArticleDto;
 import com.fc8.projectboard.dto.ArticleWithCommentsDto;
 import com.fc8.projectboard.dto.UserDto;
 import com.fc8.projectboard.repository.ArticleRepository;
+import com.fc8.projectboard.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -35,6 +37,9 @@ class ArticleServiceTest {
 
     @Mock
     private ArticleRepository articleRepository;
+
+    @Mock
+    private UserRepository userRepository;
 
     @DisplayName(value = "검색어 없이 게시글을 검색하면, 게시글 페이지를 반환한다.")
     @Test
@@ -100,6 +105,42 @@ class ArticleServiceTest {
         then(articleRepository).should().findByHashtag(hashtag, pageable);
     }
 
+    @DisplayName("게시글 ID로 조회하면, 댓글 달긴 게시글을 반환한다.")
+    @Test
+    void givenArticleId_whenSearchingArticleWithComments_thenReturnsArticleWithComments() {
+        // Given
+        Long articleId = 1L;
+        Article article = createArticle();
+        given(articleRepository.findById(articleId)).willReturn(Optional.of(article));
+
+        // When
+        ArticleWithCommentsDto dto = articleService.getArticleWithComments(articleId);
+
+        // Then
+        assertThat(dto)
+                .hasFieldOrPropertyWithValue("title", article.getTitle())
+                .hasFieldOrPropertyWithValue("content", article.getContent())
+                .hasFieldOrPropertyWithValue("hashtag", article.getHashtag());
+        then(articleRepository).should().findById(articleId);
+    }
+
+    @DisplayName("댓글 달린 게시글이 없으면, 예외를 던진다.")
+    @Test
+    void givenNonexistentArticleId_whenSearchingArticleWithComments_thenThrowsException() {
+        // Given
+        Long articleId = 0L;
+        given(articleRepository.findById(articleId)).willReturn(Optional.empty());
+
+        // When
+        Throwable t = catchThrowable(() -> articleService.getArticleWithComments(articleId));
+
+        // Then
+        assertThat(t)
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessage("게시글이 없습니다 - articleId: " + articleId);
+        then(articleRepository).should().findById(articleId);
+    }
+
     @DisplayName(value = "게시글을 조회하면, 게시글을 반환")
     @Test
     void givenArticleId_whenSearchingArticle_thenReturnsArticle() {
@@ -109,7 +150,7 @@ class ArticleServiceTest {
         given(articleRepository.findById(articleId)).willReturn(Optional.of(article));
 
         // when
-        ArticleWithCommentsDto dto = articleService.getArticle(articleId);
+        ArticleDto dto = articleService.getArticle(articleId);
 
         // then
         assertThat(dto).hasFieldOrPropertyWithValue("title", article.getTitle())
@@ -141,12 +182,14 @@ class ArticleServiceTest {
     void givenArticleInfo_whenSavingArticle_thenSavesArticle() {
         // given
         ArticleDto dto = createArticleDto();
+        given(userRepository.getReferenceById(dto.userDto().id())).willReturn(createUser());
         given(articleRepository.save(any(Article.class))).willReturn(createArticle());
 
         // when
         articleService.saveArticle(dto);
 
         // then
+        then(userRepository).should().getReferenceById(dto.userDto().id());
         then(articleRepository).should().save(any(Article.class));
 
     }
@@ -160,7 +203,7 @@ class ArticleServiceTest {
         given(articleRepository.getReferenceById(dto.id())).willReturn(article);
 
         // when
-        articleService.updateArticle(dto);
+        articleService.updateArticle(dto.id(), dto);
 
         // then
         assertThat(article)
@@ -179,7 +222,7 @@ class ArticleServiceTest {
         given(articleRepository.getReferenceById(dto.id())).willThrow(EntityNotFoundException.class);
 
         // when
-        articleService.updateArticle(dto);
+        articleService.updateArticle(dto.id(), dto);
 
         // then
         then(articleRepository).should().getReferenceById(dto.id());
@@ -244,12 +287,16 @@ class ArticleServiceTest {
     }
 
     private Article createArticle() {
-        return Article.of(
+        Article article = Article.of(
                 createUser(),
                 "title",
                 "content",
                 "#java"
         );
+
+        ReflectionTestUtils.setField(article, "id", 1L);
+
+        return article;
     }
 
     private ArticleDto createArticleDto() {
